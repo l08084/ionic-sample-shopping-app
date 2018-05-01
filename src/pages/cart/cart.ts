2dd438b5bc3d35ced00bb28228ba163c564c5365
l08084 <l08084.1989@gmail.com>
2018-05-01 22:29:19 +0900

    カート内に商品を追加する処理を追加

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -17,7 +17,57 @@ export class CartPage {
     public navCtrl: NavController,
     public alertCtrl: AlertController,
     private storage: Storage,
-    public events: Events,
     private productProvider: ProductProvider
   ) {}
+
+  ionViewDidEnter() {
+    this.productList = [];
+    this.subtotal = 0;
+    this.isEmpty = false;
+    this.storage
+      .length()
+      .then(result => {
+        if (result > 0) {
+          this.storage
+            .get("items")
+            .then(result => {
+              this.productList = result;
+              this.productList.forEach(
+                product => (this.subtotal += product.price)
+              );
+            })
+            .catch(err => {});
+        } else {
+          this.isEmpty = true;
+        }
+      })
+      .catch(err => {});
+  }
+
+  order() {
+    const confirm = this.alertCtrl.create({
+      title: "注文を確定しますか？",
+      buttons: [
+        {
+          text: "キャンセル",
+          handler: () => {}
+        },
+        {
+          text: "OK",
+          handler: () => {
+            let alert = this.alertCtrl.create({
+              title: "ご注文を受け付けました！",
+              subTitle: "ご指定の住所までのお届け時間は、20-30分程です",
+              buttons: ["OK"]
+            });
+            alert.present();
+            this.storage.clear();
+            this.productList = [];
+            this.isEmpty = true;
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
