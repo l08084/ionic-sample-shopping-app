e714077c069a171406a58e561ffff471efb0a67c
l08084 <l08084.1989@gmail.com>
2018-05-03 20:48:57 +0900

    tab側でsubscribe

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 import { HomePage } from "../home/home";
 import { CartPage } from "../cart/cart";
+import { Events } from "ionic-angular";
 
 @Component({
   templateUrl: "tabs.html"
@@ -9,6 +10,14 @@ import { CartPage } from "../cart/cart";
 export class TabsPage {
   public tab1Root = HomePage;
   public tab2Root = CartPage;
+  public itemCount = 0;
 
-  constructor() {}
+  constructor(public events: Events) {}
+
+  ionViewDidLoad() {
+    this.events.subscribe(
+      "cart:updated",
+      count => (this.itemCount = count ? count : 0)
+    );
+  }
 }
