467e48a39c46b3d9203c71d127aaa958ca38ca99
l08084 <l08084.1989@gmail.com>
2018-05-05 23:21:52 +0900

    デフォルトのページを削除する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
 import { MyApp } from "./app.component";
 
-import { AboutPage } from "../pages/about/about";
-import { ContactPage } from "../pages/contact/contact";
 import { HomePage } from "../pages/home/home";
 import { TabsPage } from "../pages/tabs/tabs";
 
@@ -18,15 +16,7 @@ import { CartPage } from "../pages/cart/cart";
 import { IonicStorageModule } from "@ionic/storage";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    DetailPage,
-    CartPage
-  ],
+  declarations: [MyApp, HomePage, TabsPage, DetailPage, CartPage],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
@@ -34,15 +24,7 @@ import { IonicStorageModule } from "@ionic/storage";
     IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    DetailPage,
-    CartPage
-  ],
+  entryComponents: [MyApp, HomePage, TabsPage, DetailPage, CartPage],
   providers: [
     StatusBar,
     SplashScreen,
